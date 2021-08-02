427be8f9d2b048478cabc72d412fa5724d40fc5b
Koulinn <drdverzola@gmail.com>
2021-08-02 08:55:08 +0100

    created div container to create BG for NavBar on scroll

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -5,33 +5,36 @@ import { MdShare, MdMenu } from "react-icons/md";
 
 function topNavBar() {
     return (
-        <Navbar expand="md" fixed="top" variant="light">
-            <Container>
-                <div>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" className="border-0"> <MdMenu></MdMenu></Navbar.Toggle>
-                    <Navbar.Brand href="#home" className="pl-3">
-                        <div className="d-none d-md-flex flex-column justify-content-center align-items-center">
-                            <Logo></Logo>
-                            <span>Front-End/UX Designer</span>
-    
-                        </div>
-                        <div className="d-flex d-md-none">
-                            <span>Rafa</span>
-    
-                        </div>
-                    
-                    </Navbar.Brand>
-                </div>
-               <MdShare className="d-md-none"></MdShare>
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="mx-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#about">About</Nav.Link>
-                        <Nav.Link href="#portfolio">Portfolio</Nav.Link>
-                        <Nav.Link href="#resume">Resume</Nav.Link>
-                        <Nav.Link href="#contact">Contact</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
+        <Navbar expand="md" fixed="top" variant="light" className="p-0 py-2">
+            <Container fluid className="position-relative" className="p-0">
+            <div id="BG-NavBar"></div>
+                <Container className="p-0">
+                    <div>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" className="border-0"> <MdMenu></MdMenu></Navbar.Toggle>
+                        <Navbar.Brand href="#home" className="pl-3">
+                            <div className="d-none d-md-flex flex-column justify-content-center align-items-center">
+                                <Logo></Logo>
+                                <span>Front-End/UX Designer</span>
+        
+                            </div>
+                            <div className="d-flex d-md-none">
+                                <span>Rafa</span>
+        
+                            </div>
+                        
+                        </Navbar.Brand>
+                    </div>
+                   <MdShare className="d-md-none"></MdShare>
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mx-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#about">About</Nav.Link>
+                            <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+                            <Nav.Link href="#resume">Resume</Nav.Link>
+                            <Nav.Link href="#contact">Contact</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
             </Container>
         </Navbar>
     )
