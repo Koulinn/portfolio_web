b0eadf01b7201200d7161687ec2a1be70aaaad84
Koulinn <drdverzola@gmail.com>
2021-08-02 13:42:53 +0100

    Mediaquery sent to app comp

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,17 @@
-import {Container} from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
 import Hero from './hero/Hero'
 import NavBar from './navBar/NavBar'
 import '../styles/styles.css'
+import { useMediaQuery } from 'react-responsive'
 
 function App() {
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 767px)' })
+  const isMobile = useMediaQuery({ query: '(max-width: 576px)' })
   return (
     <Container className="p-0" fluid>
-      <NavBar></NavBar>
-      <Hero></Hero>
-      
+      <NavBar isTabletOrMobile={isTabletOrMobile} isMobile={isMobile}></NavBar>
+      <Hero heroTextFirst={"Hello, I’m Rafa!"} heroTextSpan={"<br>"} heroTextSecond="Welcome"></Hero>
+
     </Container>
   );
 }
