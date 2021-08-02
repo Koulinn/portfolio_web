0db0365c80fb8b9565bbde2dccb385ed8e33fe20
Koulinn <drdverzola@gmail.com>
2021-08-02 07:34:23 +0100

    Imported Hero and stylesheet

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import {Container} from 'react-bootstrap'
+import Hero from './hero/Hero'
 import NavBar from './navBar/NavBar'
+import '../styles/styles.css'
 
 function App() {
   return (
     <Container className="p-0" fluid>
       <NavBar></NavBar>
+      <Hero></Hero>
       
     </Container>
   );
