d615b645232b49a62bc274933b7d2094ccae1194
Koulinn <drdverzola@gmail.com>
2021-08-02 06:11:24 +0100

    set padding from global container to 0

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
+import {Container} from 'react-bootstrap'
+import NavBar from './navBar/NavBar'
+
 function App() {
   return (
-    <>
+    <Container className="p-0" fluid>
+      <NavBar></NavBar>
       
-    </>
+    </Container>
   );
 }
 
