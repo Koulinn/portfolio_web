5404f1fc04415ec28b7c97127e363b0948c73e34
Koulinn <drdverzola@gmail.com>
2021-08-03 09:03:50 +0100

    Created Home Component

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Hero from '../hero/Hero'
+import BGBody from '../shared/BGBody'
 
-function Home() {
+function Home(props) {
     return (
-        <div>
-            
-        </div>
+        <main>
+            <Hero isTabletOrMobile={props.isTabletOrMobile} isMobile={props.isMobile} heroTextFirst={"Hello, I’m Rafa!"} heroTextSpan={"<br>"} heroTextSecond="Welcome"></Hero>
+            <Container className="p-0 position-relative" fluid>
+                <BGBody className=""></BGBody>
+            </Container>
+
+
+        </main>
     )
 }
 
