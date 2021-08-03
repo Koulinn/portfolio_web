55272b8caee2843245bdf60511abb6456bcfdd38
Koulinn <drdverzola@gmail.com>
2021-08-03 10:12:49 +0100

    BGBody zindex -1

--- src/components/shared/BGBody.jsx
+++ src/components/shared/BGBody.jsx
@@ -5,7 +5,7 @@ import { ReactComponent as CurvedBG } from '../../assets/BG/BGBody.svg'
 
 function BGBody() {
     return (
-        <div className="position-absolute w-100">
+        <div className="position-absolute w-100" style={{zIndex:"-1"}}>
             <CurvedBG></CurvedBG>
             
         </div>
