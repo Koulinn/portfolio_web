9a34228a1bdd2ed1327e948956a12088114924e7
Koulinn <drdverzola@gmail.com>
2021-08-03 14:12:06 +0100

    Added BG to the Contact comp

--- src/components/shared/Contact/Contact.jsx
+++ src/components/shared/Contact/Contact.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { Container, Row } from 'react-bootstrap'
+import UnitedImg from '../../../assets/Imgs/united.png'
+import BGBody from '../BGBody'
 
 function Contact() {
     return (
-        <div>
-            
-        </div>
+        <Container id="contact-section" className="p-0 position-relative" fluid>
+            <BGBody></BGBody>
+            <Container>
+                <Row>
+                    <div className="col-12 col-md-6">
+    
+                    </div>
+    
+                    <div className="col-12 col-md-6">
+                        <img className="ImgShadowEffect" src={UnitedImg} alt="" />
+                    </div>
+                </Row>
+                
+            </Container>
+        </Container>
     )
 }
 
