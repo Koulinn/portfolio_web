e0e8d5b7bb08bddfa98205465d0f62faa099ced6
Koulinn <drdverzola@gmail.com>
2021-08-03 15:27:35 +0100

    Create a Contact Text comp to handle text part

--- src/components/shared/Contact/Contact.jsx
+++ src/components/shared/Contact/Contact.jsx
@@ -2,18 +2,20 @@ import React from 'react'
 import { Container, Row } from 'react-bootstrap'
 import UnitedImg from '../../../assets/Imgs/united.png'
 import BGBody from '../BGBody'
+import ContactText from './ContactText'
 
-function Contact() {
+
+function Contact(props) {
     return (
         <Container id="contact-section" className="p-0 position-relative" fluid>
             <BGBody></BGBody>
             <Container>
                 <Row>
                     <div className="col-12 col-md-6">
-    
+                        <ContactText spanText={props.spanText}></ContactText>
                     </div>
     
-                    <div className="col-12 col-md-6">
+                    <div className=" d-none col-12 col-md-6 d-md-flex justify-content-md-end">
                         <img className="ImgShadowEffect" src={UnitedImg} alt="" />
                     </div>
                 </Row>
