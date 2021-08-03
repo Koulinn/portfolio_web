fd08cca71360f274848f9d531cc4652ffd370e98
Koulinn <drdverzola@gmail.com>
2021-08-03 09:03:05 +0100

    Passed isTabletOrMobile and isMobile props to the pages

--- src/components/shared/Pages.jsx
+++ src/components/shared/Pages.jsx
@@ -3,16 +3,19 @@ import About from '../about/About'
 import Home from '../home/Home'
 import Portfolio from '../porfolio/Portfolio'
 import Resume from '../resume/Resume'
+import { Container } from 'react-bootstrap'
+import BGBody from './BGBody'
 
 
-function Pages() {
+function Pages(props) {
     return (
         <>
-        <Home></Home>
-        <About></About>
-        <Portfolio></Portfolio>
-        <Resume></Resume>
-            
+           
+            <Home isTabletOrMobile={props.isTabletOrMobile} isMobile={props.isMobile}></Home>
+            <About isTabletOrMobile={props.isTabletOrMobile} isMobile={props.isMobile}></About>
+            <Portfolio isTabletOrMobile={props.isTabletOrMobile} isMobile={props.isMobile}></Portfolio>
+            <Resume isTabletOrMobile={props.isTabletOrMobile} isMobile={props.isMobile}></Resume>
+
         </>
     )
 }
