918c1f1d17352ad9bbc7941570b0f19b0170be6c
Koulinn <drdverzola@gmail.com>
2021-08-03 10:15:36 +0100

    created Custom Carousel Dot

---
+++ src/components/shared/CustomCarouselDot.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CustomCarouselDot() {
+    return (
+        <div className="customCarouselDot">
+            
+        </div>
+    )
+}
+
+export default CustomCarouselDot
