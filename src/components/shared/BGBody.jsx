853cc194fe6a81baf98594f7f272906488bb188b
Koulinn <drdverzola@gmail.com>
2021-08-03 14:31:03 +0100

    Rotate Contact BG

--- src/components/shared/BGBody.jsx
+++ src/components/shared/BGBody.jsx
@@ -6,7 +6,7 @@ import { ReactComponent as CurvedBG } from '../../assets/BG/BGBody.svg'
 function BGBody() {
     return (
         <div className="position-absolute w-100" style={{zIndex:"-1"}}>
-            <CurvedBG></CurvedBG>
+            <CurvedBG className="BGRotate"></CurvedBG>
             
         </div>
     )
