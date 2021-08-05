8abf756711056814576e88afdfc5a1e363e69b4e
Koulinn <drdverzola@gmail.com>
2021-08-05 09:56:17 +0100

    Create FooterNav comp

---
+++ src/components/shared/footer/FooterNav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function FooterNav() {
+    return (
+        <nav>
+            <ul className="pl-0">
+                <li>About</li>
+                <li className="my-2">Portfolio</li>
+                <li>Resume</li>
+                <li className="mt-2">Contact</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default FooterNav
