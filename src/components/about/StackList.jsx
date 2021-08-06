e6da763e0fbeecfad4bf6da39747cff7c4059cb5
Koulinn <drdverzola@gmail.com>
2021-08-06 07:39:18 +0100

    created StackList comp

---
+++ src/components/about/StackList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function StackList(props) {
+    return (
+        <div className="col-12 col-md-6">
+            <h4 className="text-center">{props.title}</h4>
+        </div>
+    )
+}
+
+export default StackList
