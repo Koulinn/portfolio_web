e2828f9652a4fcfbca56ce2717e21a1e722f7a0b
Koulinn <drdverzola@gmail.com>
2021-08-06 14:07:51 +0100

    Effect to List section added

--- src/components/about/StackList.jsx
+++ src/components/about/StackList.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import { useScrollYPosition } from 'react-use-scroll-position';
+
 
 function StackList(props) {
+    const scrollY = useScrollYPosition();
     return (
-        <div className="col-12 col-md-6">
-            <h4 className="text-center">{props.title}</h4>
-            <ul className="d-flex flex-wrap flex-column">
-                {props.list.map((skill, i) => <li key={i}>{skill}</li>)}
-                {props.list.map((skill, i) => <li key={i}>{skill}</li>)}
-            </ul>
+        <div className={scrollY < 1000 || scrollY > 2000 ? "col-12 col-md-6 sideFadeEffect" : "col-12 col-md-6 sideFadeEffect sideFadeEffectShow"}>
+                       
+            <div>
+                    <h4 className="text-center">{props.title} {console.log(scrollY)}</h4>
+                    <ul className="d-flex flex-wrap flex-column">
+                        {props.list.map((skill, i) => <li key={i}>{skill}</li>)}
+                        {props.list.map((skill, i) => <li key={i}>{skill}</li>)}
+                    </ul>
+            </div>
+            
         </div>
     )
 }
