8a5179d388359f9d627fc8fbc31ab6b96fb087d3
Koulinn <drdverzola@gmail.com>
2021-09-17 18:46:07 +0100

    Added CV section

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -14,11 +14,18 @@ function Resume(props) {
             <Container className="p-0 position-relative" fluid>
                 <BGTop></BGTop>
                 <Container>
-                    <CommonSection order="0" spanText="<br>"></CommonSection>
-                    <TimeLine/>
-                    <CommonSection order={props.isTabletOrMobile ? "0" : "2"} spanText="<br>"></CommonSection>
+                    <CommonSection
+                        imgURL={'https://res.cloudinary.com/koulin/image/upload/v1631900496/Portfolio/CV_ov15mg.jpg'}
+                        isCarousel={false}
+                        title={'Check out my CV'}
+                        text={'A well rounded Web UX desinerloper with great leadership skills, creativity and always read to play in any team! '}
+                        order="0"
+                        spanText="<br>"
+                        linkUrl='https://www.figma.com/file/o22eUJF3N1mkTbP2JNLhZt/Rafa-Lima->-Portfolio?node-id=0%3A1'
+                        linkText={`Download Rafa Lima CV`}
 
-
+                    ></CommonSection>
+                    <TimeLine />
                 </Container>
             </Container>
 
