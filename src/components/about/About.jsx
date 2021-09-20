9afa784ae3f4ce855bbda1fd60550ea31cbe9bbe
Koulinn <drdverzola@gmail.com>
2021-09-20 15:44:53 +0100

    added text to about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -9,18 +9,27 @@ import Stack from './Stack'
 function About(props) {
     return (
         <>
-            <Hero isTabletOrMobile={props.isTabletOrMobile} isMobile={props.isMobile} heroTextFirst={"I'm quite..."} heroTextSpan={"<br>"} heroTextSecond="curious"></Hero>
+            <Hero isTabletOrMobile={props.isTabletOrMobile} isMobile={props.isMobile} heroTextFirst={"Curiosity"} heroTextSpan={"<br>"} heroTextSecond="drives me"></Hero>
             <Container className="p-0 position-relative" fluid>
                 <BGTop></BGTop>
                 <Container>
                     <CommonSection
-                        title={'Home is still under development'}
-                        text={'this page is under development'}
+                        title={`I'm always learning something new`}
+                        text={'I love changing environments where we need to be always researching and adapting to achieve the best results and deliver an outstanding project'}
                         order="0"
                         spanText="<br>">
 
                     </CommonSection>
-                    <CommonSection order={props.isTabletOrMobile ? "0" : "2"} spanText="<br>"></CommonSection>
+                    <CommonSection
+                        order={props.isTabletOrMobile ? "0" : "2"}
+                        spanText="<br>"
+                        title={`Discover the unknown`}
+                        text={`I'm always changing my habits but I found martial arts a place to keep balance between modern life and nature`}
+
+
+                    >
+
+                    </CommonSection>
                     <Stack spanText="<br>"></Stack>
 
                 </Container>
