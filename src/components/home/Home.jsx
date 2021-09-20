369558f0529bafa7efea59c9ccf430afea438124
Koulinn <drdverzola@gmail.com>
2021-09-20 19:01:37 +0100

    added first section to home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,6 +3,7 @@ import { Container } from 'react-bootstrap'
 import Hero from '../hero/Hero'
 import CommonSection from '../shared/CommonSection'
 import BGTop from '../shared/BGTop'
+import { Link } from 'react-router-dom'
 
 
 function Home(props) {
@@ -12,13 +13,21 @@ function Home(props) {
             <Container className="p-0 position-relative" fluid>
                 <BGTop></BGTop>
                 <Container>
-                    <CommonSection
-                        title={'Home is still under development'}
-                        text={'this page is under development'}
-                        order="0"
-                        spanText="<br>">
 
+                    <CommonSection
+                        title={'A well rounded professional with UX and Full-stack development skills'}
+                        text={`Check out my UX and Web development projects`}
+                        order="0"
+                        spanText="<br>"
+                        imgURL={'https://res.cloudinary.com/koulin/image/upload/v1632159321/Portfolio/bigstock-Lego-Workers_1_y3wssk.jpg'}
+                        isCarousel={false}
+                        internalUrl='/portfolio'
+                        linkText={`Go to portfolio`}
+                    >
                     </CommonSection>
+
+
+
                     <CommonSection
                         title={'Home is still under development'}
                         text={'this page is under development'}
