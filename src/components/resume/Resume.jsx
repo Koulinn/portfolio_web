fd5329e0de2ae462b62e4fe1addc7f56c4e937df
Koulinn <drdverzola@gmail.com>
2021-10-05 19:58:13 +0100

    updated dev title

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -21,7 +21,7 @@ function Resume(props) {
                         text={'A well rounded Web UX desinerloper with great leadership skills, creativity and always ready to play in any team! '}
                         order="0"
                         spanText="<br>"
-                        linkUrl='https://1drv.ms/b/s!AnfHEZPn_w1DrOUo1AMUBcEKBjCIZw'
+                        linkUrl='https://1drv.ms/b/s!AnfHEZPn_w1DrOUrOwnMZ9bu6r2Eog?e=zYNR7C'
                         linkText={`Download Rafa Lima CV`}
                         download={true}
 
