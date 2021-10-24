1d954550e985f3f51959b5b6f75ab90f0376a36e
Koulinn <drdverzola@gmail.com>
2021-10-24 08:24:12 +0100

    updated portflio section

--- src/components/porfolio/Portfolio.jsx
+++ src/components/porfolio/Portfolio.jsx
@@ -50,14 +50,14 @@ function Portfolio(props) {
                 <BGTop></BGTop>
                 <Container>
                     <CommonSection
-                        imgURL={'https://res.cloudinary.com/koulin/image/upload/v1631551108/Portfolio/0e58fde1890c02624cc2e4bd378e72b07255c8ff-4240x2000_fq7aew.png'}
+                        imgURL={'https://res.cloudinary.com/koulin/image/upload/v1635060036/Portfolio/portfolio_img_iu6obo.jpg'}
                         isCarousel={false}
                         title={'UX projects'}
-                        text={'Have a peek at my canvas with my projects I have created'}
+                        text={'Check out my UX portfolio with my projects I participated'}
                         order="0"
                         spanText="<br>"
-                        linkUrl='https://www.figma.com/file/o22eUJF3N1mkTbP2JNLhZt/Rafa-Lima->-Portfolio?node-id=0%3A1'
-                        linkText={`Go to Rafa Lima's FigJam board`}
+                        linkUrl='https://drive.google.com/file/d/1FNc4KKIHjcGmSCyByAA6Dl1kSlQZZ4p1/view?usp=sharing'
+                        linkText={`Download Rafa Lima Portfolio`}
                         subsection={<List items={items} />}
                     >
 
